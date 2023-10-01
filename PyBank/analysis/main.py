849ff9bd21ea3d33bfa1ce38258d1d1340bf820a
mamathaetikyala <mamatha.ereddy@gmail.com>
2023-10-01 09:17:59 -0400

    second version

--- PyBank/analysis/main.py
+++ PyBank/analysis/main.py
@@ -1,6 +1,63 @@
+import os
+# Module for reading CSV files
 import csv
 
-print("pyBank Project")
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 
-#with open('budget_data.csv', newline='') as csvfile:
+
+def Average(dict):
+    sum_of_averages = 0
+    len_of_averages = 0
+    for k, v in dict.items():
+        sum_of_averages += v
+        len_of_averages += 1
+    return sum_of_averages/len_of_averages
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
     
+    #getting 2nd row
+    pre_lne = next(csvreader)
+    total_months = 1
+    total =  int(pre_lne[1])
+    changes = {}
+
+    #starting form 3rd row
+    for row in csvreader:
+        total_months += 1
+        total += int(row[1])
+        changes[row[0]] = int(row[1]) - int(pre_lne[1])
+        pre_lne = row #storing current row as a previous row
+        
+    #get the corresponding key for max and min value of the dic
+    greatest_inc_month = max(changes, key = lambda x: changes[x] )
+    greatest_dec_month = min(changes, key = lambda x: changes[x] )
+    greatest_profit_inc = "${:.0f}".format(changes[greatest_inc_month] )
+    greatest_profit_dec = "${:.0f}".format(changes[greatest_dec_month] )
+    Average_changes = "${:.2f}".format(Average(changes))
+    
+    
+    print('Financial Analysis ')
+    print('------------------------- ')
+    print('Total Months: ',total_months)
+    print('Total:', "${:.0f}".format(total))
+    print('Average Change:',Average_changes)
+    print('Greatest Increase in Profits:',greatest_inc_month,greatest_profit_inc)
+    print('Greatest Decrease in Profits:',greatest_dec_month,greatest_profit_dec)
+    
+    with open('output.txt', 'w') as op_file:
+        op_file.write('Financial Analysis \n \n')
+        op_file.write('------------------------- \n \n')
+        op_file.write('Total Months: %s \n \n' %(total_months))
+        #op_file.write('------------------------- \n \n')
+        op_file.write('Total: %s \n \n' %("${:.0f}".format(total)))
+        #op_file.write('------------------------- \n \n')
+        op_file.write('Average Change: %s \n \n' %(Average_changes))
+        #op_file.write('------------------------- \n \n')
+        op_file.write("Greatest Increase in Profits: %s (%s)\n \n" % (greatest_inc_month, greatest_profit_inc))
+        op_file.write("Greatest Decrease in Profits: %s (%s)\n \n" % (greatest_dec_month, greatest_profit_dec))
--- PyPoll/analysis/main.py
+++ PyPoll/analysis/main.py
@@ -1,37 +1,67 @@
 # First we'll import the os module
 # This will allow us to create file paths across operating systems
 import os
-
 # Module for reading CSV files
 import csv
 
 csvpath = os.path.join('..', 'Resources', 'election_data.csv')
 
-#print(csvpath)
-
-# # Method 1: Plain Reading of CSV files
-# with open(csvpath, 'r') as file_handler:
-#     lines = file_handler.read()
-#     print(lines)
-#     print(type(lines))
-
-
-# Method 2: Improved Reading using CSV module
 
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    print(csvreader)
-
+ 
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
+    
+    total_votes = 0
+    candidates = {}
+    
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
+        total_votes += 1
+        if row[2] not in candidates.keys():
+            candidates[row[2]] = 1
+        else:    
+            candidates[row[2]] += 1
+    
+    print('Election Results \n')
+    print('------------------------- \n')
+    print('Total Votes: ',total_votes)
+    print('------------------------- \n')
+    for (k, v) in candidates.items():
+        print( str(k) , ' : ', format(v/total_votes, ".3%"),"(", str(v) ,")")
+        
+    print('------------------------- \n')
+    Winner = max(candidates, key = lambda x: candidates[x] )
+    print('Winner:', Winner)
+    
+    
+with open('output.txt', 'w') as op_file:
+    op_file.write('Election Results \n \n')
+    op_file.write('------------------------- \n \n')
+    op_file.write('Total Votes: %s \n \n' %(total_votes))
+    op_file.write('------------------------- \n \n')
+    for (k, v) in candidates.items():
+        op_file.write("%s: %s (%s)\n \n" % (k, format(v/total_votes, ".3%"), v))
+        
+    op_file.write('------------------------- \n \n')
+    op_file.write('Winner: %s \n \n '%(Winner))
+    op_file.write('------------------------- ')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
